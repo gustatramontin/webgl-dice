7e66a989cd6381f3cfa8d2877c3b2894c8c55239
Gustavo <mrgustavotramontin@gmail.com>
2023-11-23 11:32:32 -0300

    deleted old code

--- webgl.js
+++ webgl.js
@@ -87,51 +87,6 @@ function makeBuffer(gl, type, data)
 	return buffer
 }
 
-function initIndexBuffer(gl) 
-{
-
-	const indices = [
-		0,
-		1,
-		2,
-		0,
-		2,
-		3, // front
-		4,
-		5,
-		6,
-		4,
-		6,
-		7, // back
-		8,
-		9,
-		10,
-		8,
-		10,
-		11, // top
-		12,
-		13,
-		14,
-		12,
-		14,
-		15, // bottom
-		16,
-		17,
-		18,
-		16,
-		18,
-		19, // right
-		20,
-		21,
-		22,
-		20,
-		22,
-		23, // left
-	];
-
-	return makeBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices))
-
-}
 function createShader(gl,type,source)
 {
 	const shader = gl.createShader(type)
@@ -189,7 +144,6 @@ async function init()
 	program = createProgram(gl, vertex_shader, fragment_shader)
 
 	attributes.position = gl.getAttribLocation(program, "a_position")
-	//attributes.colors = gl.getAttribLocation(program, "a_color")
 	attributes.texture = gl.getAttribLocation(program, "a_texcoord")
 
 	uniforms.transform = gl.getUniformLocation(program, "transform")
@@ -198,51 +152,10 @@ async function init()
 
 	const model = await loadModel()
 	console.log("loaded model")
-	buffers.index = makeBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.indexes)) //initIndexBuffer(gl)
+	buffers.index = makeBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.indexes)) 
 
 	buffers.position = makeBuffer(gl, gl.ARRAY_BUFFER,new Float32Array(model.vertices))
 
-	/*buffers.colors = makeBuffer(gl, gl.ARRAY_BUFFER, new Float32Array([
-		[1,0,0,1],
-		[1,1,0,1],
-		[0,1,0,1],
-		[1,0,1,1],
-		[0,0,1,1],
-		[0,1,1,1],
-	].flatMap(x => [x,x,x,x].map(x => x.flat())).flat()))*/
-
-	const texcoord = [
-	//	1
-	0,1/3,
-	0,2/3,
-	1/4,2/3,
-	1/4,1/3,
-	//	2
-	1/4,1/3,
-	1/4,2/3,
-	2/4,2/3,
-	2/4,1/3,
-	//	3
-	2/4,1/3,
-	2/4,2/3,
-	3/4,2/3,
-	2/3,1/3,
-	//	4
-	3/4,1/3,
-	3/4,2/3,
-	1,2/3,
-	1,1/3,
-	//	5
-	1/4,2/3,
-	1/4,1,
-	2/4,1,
-	2/4,2/3,
-	//	6
-	1/4,0,
-	1/4,1/3,
-	2/4,1/3,
-	2/4,0,
-	]
 	buffers.texture = makeBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(model.uvs))
 
 	texture = gl.createTexture()
@@ -287,13 +200,6 @@ function draw(dt)
 	const voffset = 0
 
 	gl.vertexAttribPointer(attributes.position, size, type, normalize, stride, voffset)
-
-	/*
-	gl.enableVertexAttribArray(attributes.colors)
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffers.colors);
-
-	gl.vertexAttribPointer(attributes.colors, 4, gl.FLOAT, false, 0, 0)
-	*/
 
 	gl.enableVertexAttribArray(attributes.texture)
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffers.texture)
